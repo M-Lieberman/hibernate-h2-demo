61284d71d5d2672cfb3dc116ba35bb0ae95688d3
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-11-29 10:50:02 +0000

    Add accessor methods and default constructor to Person

--- src/main/java/com/hibernate/demo/Person.java
+++ src/main/java/com/hibernate/demo/Person.java
@@ -1,22 +1,48 @@
 package com.hibernate.demo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Person {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column(name="FULL_NAME")
+    @Column(name = "FULL_NAME")
     private String name;
     private int age;
+
+    // Hibernate needs a default constructor
+    public Person() {
+    }
 
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
